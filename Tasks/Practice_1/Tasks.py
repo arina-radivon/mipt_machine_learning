5d4aa82d09fbbe260a90be4bb80289102371580b
Arina <arina-radivon@ya.ru>
2022-11-19 16:51:53 +0300

    New commit

---
+++ Tasks/Practice_1/Tasks.py
@@ -0,0 +1,23 @@
+""" Task 1 """
+
+name = str(input())
+print('Привет, ', name, '!', sep='')
+
+
+""" Task 2 """
+
+a = int(input())
+print(a-1)
+print(a)
+print(a+1)
+
+
+""" Task 3 """
+
+name = str(input())
+animal = str(input())
+breed = str(input())
+age = int(input())
+print('Ваш питомец:', animal, name)
+print('Порода питомца:', breed)
+print('Возраст питомца:', age)
